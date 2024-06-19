6788c9f8d5b13eba27c3ced2b59f660a85e4c181
chirasoft <chirasof@gmail.com>
2024-06-18 20:07:55 -0600

    video 9

--- funciones.js
+++ funciones.js
@@ -5,3 +5,9 @@ function doGet(){
 function obtenerDatosHtml(nombre){
     return HtmlService.createHtmlOutputFromFile(nombre).getContent();
 }
+function obtenerContactos(){
+    let hoja = SpreadsheetApp.openById('1EBe1Ow2GtPeK4_qK4GGNjv2ylqKeUpNNKH_z_iSGEH4').getActiveSheet();
+    let datos = hoja.getDataRange().getValues();
+    Logger.log(datos);
+    return datos;
+}
