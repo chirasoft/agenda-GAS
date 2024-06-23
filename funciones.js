9911a87a9c3fe738b5f4e07aaf05d664f643ca85
chirasoft <chirasof@gmail.com>
2024-06-23 11:22:44 -0600

    video 15

--- funciones.js
+++ funciones.js
@@ -1,13 +1,28 @@
-function doGet(){
-    return HtmlService.createTemplateFromFile('web').evaluate().setTitle('Agenda GAS');
-  
+
+const HOJA = SpreadsheetApp.openById('1EBe1Ow2GtPeK4_qK4GGNjv2ylqKeUpNNKH_z_iSGEH4').getActiveSheet();
+function doGet(datos)
+{
+    //insertarContactoGs(datos.parameter.nombreContacto,datos.parameter.correoContacto);
+    return HtmlService.createTemplateFromFile('web').evaluate().setTitle('Agenda GAS');  
 }
-function obtenerDatosHtml(nombre){
+
+function doPost(datos)
+{
+ //   insertarContactoGs(datos.parameter.nombreContacto,datos.parameter.correoContacto);
+    return HtmlService.createTemplateFromFile('web').evaluate().setTitle('Agenda GAS');
+}
+
+function obtenerDatosHtml(nombre)
+{
     return HtmlService.createHtmlOutputFromFile(nombre).getContent();
 }
-function obtenerContactos(){
-    let hoja = SpreadsheetApp.openById('1EBe1Ow2GtPeK4_qK4GGNjv2ylqKeUpNNKH_z_iSGEH4').getActiveSheet();
-    let datos = hoja.getDataRange().getValues();
-    Logger.log(datos);
-    return datos;
+
+function obtenerContactos()
+{   
+    return HOJA.getDataRange().getValues();
+}
+
+function insertarContactoGs (nombre,correo)
+{
+    HOJA.appendRow([nombre,correo]);
 }
